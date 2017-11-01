a673c5c494aabae37b3ad9023192a7638da7c0be
Artyom Lisovskij <artyom@lisovskij.ru>
2017-11-01 10:06:13 +0400

    init
    initial

---
+++ NewToken.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.11;
+
+import "./ERC20Standard.sol";
+
+contract NewToken is ERC20Standard {
+	function NewToken() {
+		totalSupply = 123;
+		name = "Best ever coin";
+		decimals = 4;
+		symbol = "BEC";
+		version = "1.0";
+		balances[msg.sender] = totalSupply;
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # solidity-erc20-token-bootstrap
+You are able to publish it using official Ethereum client.
+
+Russian tutorial is avaliable at vc.ru.
